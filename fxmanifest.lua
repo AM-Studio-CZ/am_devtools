c5adcf6aae07f4781b1212a033e760854e71888d
Adyss <168123142+Adyssss@users.noreply.github.com>
2026-03-15 17:16:57 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 "yes"
+
+name "adyss_devtools"
+description "Dev Tools script to help developers."
+author "Adyss"
+version "0.0.1"
+
+shared_scripts {
+	'shared/*.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
+
+server_scripts {
+	'server/*.lua'
+}
